eb1407a02e1e06c91565abc6bf2d4316a113d309
unknown <you@example.com>
2024-05-19 11:44:44 +0300

    Настроить проект

---
+++ src/shop.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<>
+			<Div>
+				<div>234</div>
+				<i class="fa fa-calendar" aria-hidden="true" />
+			</Div>
+		</>
+	);
+};
