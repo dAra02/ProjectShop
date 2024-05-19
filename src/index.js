6482f3518977be65d86aeea13353a8ca8b0a7c71
DEX <you@example.com>
2024-05-19 15:09:51 +0300

    Реализовать основной компонент блога с роутингом

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './shop';
+import { BrowserRouter } from 'react-router-dom';
+import { Shop } from './shop';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<BrowserRouter>
+		<Shop />
+	</BrowserRouter>,
 );
--- src/shop.js
+++ src/shop.js
@@ -1,16 +1,45 @@
+import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Div = styled.div`
-	text-align: center;
+const AppColumm = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 1000px;
+	min-height: 100%;
+	margin: 0 auto;
+	background-color: #fff;
 `;
 
-export const App = () => {
+const Page = styled.div`
+	padding: 120px 0;
+`;
+
+const H2 = styled.h2`
+	text-align: center;
+`;
+const Header = () => <div>Шапка</div>;
+const Footer = () => <div>Футер</div>;
+
+export const Shop = () => {
 	return (
-		<>
-			<Div>
-				<div>234</div>
-				<i class="fa fa-calendar" aria-hidden="true" />
-			</Div>
-		</>
+		<AppColumm>
+			<Header />
+
+			<Page>
+				<H2>Главная</H2>
+				<Routes>
+					<Route path="/" element={<div>Главная страница</div>} />
+					<Route path="/login" element={<div>Авторизация</div>} />
+					<Route path="/register" element={<div>Регистрация</div>} />
+					<Route path="/admin" element={<div>Админка</div>} />
+					<Route path="/corzina" element={<div>Корзина</div>} />
+					<Route path="/tovar/:tovarId" element={<div>Статья</div>} />
+					<Route path="*" element={<div>Ошибка</div>} />
+				</Routes>
+			</Page>
+
+			<Footer />
+		</AppColumm>
 	);
 };
